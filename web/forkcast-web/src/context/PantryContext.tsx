9b1b0a92c486777f72cc595fc749e040ea141f37
rahbarahsan <rahbar916@gmail.com>
2025-04-15 23:56:29 -0400

    updated pantryScreen There is bugs in view and Grocery Functionalities

--- web/forkcast-web/src/context/PantryContext.tsx
+++ web/forkcast-web/src/context/PantryContext.tsx
@@ -1,17 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import { PantryItem } from '../types';
 
 interface PantryContextType {
   items: PantryItem[];
   addItem: (item: PantryItem) => void;
+  removeItem: (id: string) => void;
   resetPantry: () => void;
 }
 
 export const PantryContext = createContext<PantryContextType>({
   items: [],
   addItem: () => {},
+  removeItem: () => {},
   resetPantry: () => {},
 });
+
+export const usePantry = () => {
+  const context = useContext(PantryContext);
+  if (context === undefined) {
+    throw new Error('usePantry must be used within a PantryProvider');
+  }
+  return context;
+};
 
 export const PantryProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [items, setItems] = useState<PantryItem[]>([]);
@@ -20,12 +30,16 @@ export const PantryProvider: React.FC<{ children: React.ReactNode }> = ({ childr
     setItems((prev) => [...prev, item]);
   };
 
+  const removeItem = (id: string) => {
+    setItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
   const resetPantry = () => {
     setItems([]);
   };
 
   return (
-    <PantryContext.Provider value={{ items, addItem, resetPantry }}>
+    <PantryContext.Provider value={{ items, addItem, removeItem, resetPantry }}>
       {children}
     </PantryContext.Provider>
   );
